8b506a6b1254d3666ab4133329b1d060a37abdf3
son123j <boy0710boy@naver.com>
2021-02-17 20:45:29 +0900

    플긴 제작자 이름

--- Easy_Survival/src/com/boy0710boy/Easy_Survival/github/plugin/Main.java
+++ Easy_Survival/src/com/boy0710boy/Easy_Survival/github/plugin/Main.java
@@ -21,7 +21,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable() {
 		System.out.println("--------------------------------------------------------------");
 		System.out.println("            Easy_Suvival 를러그인을 성공적으로 불러왔습니다");
-		System.out.println("                  플러그인 제작자 : boy0710boy");
+		System.out.println("                  플러그인 제작자 : boy0710boy, boyboy0710");
 		System.out.println("   최신버전 다운 사이트 : https://github.com/son123j/Easy_Survival");
 		System.out.println("--------------------------------------------------------------");
 		
@@ -125,7 +125,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onDisable() {
 		System.out.println("---------------------------------------------------------------");
 		System.out.println("            Easy_Suvival 를러그인이 비활성화 되었습니다");
-		System.out.println("                   플러그인 제작자 : boy0710boy");
+		System.out.println("                   플러그인 제작자 : boy0710boy, boyboy0710");
 		System.out.println("   최신버전 다운 사이트 : https://github.com/son123j/Easy_Survival");
 		System.out.println("---------------------------------------------------------------");
 	}
