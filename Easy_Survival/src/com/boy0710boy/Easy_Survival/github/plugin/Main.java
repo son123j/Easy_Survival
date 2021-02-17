91b6c1d2adefd61dbf0fe6a4520d972880ce50d3
boyboy0710 <boyboy0710@naver.com>
2021-02-17 19:38:24 +0900

    ball 조합법 추가

--- Easy_Survival/src/com/boy0710boy/Easy_Survival/github/plugin/Main.java
+++ Easy_Survival/src/com/boy0710boy/Easy_Survival/github/plugin/Main.java
@@ -95,7 +95,13 @@ public class Main extends JavaPlugin implements Listener {
 				.setIngredient('!', Material.HOPPER_MINECART)
 				.setIngredient('~', Material.EMERALD_BLOCK)
 				.setIngredient('#', Material.PHANTOM_MEMBRANE);
-		
+
+		ShapedRecipe ball = new ShapedRecipe(new ItemStack(Material.PHANTOM_SPAWN_EGG))
+				.shape(new String[]{"ABA","ACA","A A"})
+				.setIngredient('A', Material.STONE)
+				.setIngredient('B', Material.STICK)
+				.setIngredient('C', Material.GOLD_INGOT);
+
 		getServer().addRecipe(dia_pickexe);
 		getServer().addRecipe(exp_bottle);
 		getServer().addRecipe(elytra);
@@ -107,6 +113,7 @@ public class Main extends JavaPlugin implements Listener {
 		getServer().addRecipe(chicken_egg);
 		getServer().addRecipe(villager_egg);
 		getServer().addRecipe(phantom_egg);
+		//getServer().addRecipe(ball);
 	}
 	@Override
 	public void onDisable() {
